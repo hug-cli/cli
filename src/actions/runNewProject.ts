a069f4a4c3ff99b41ba4ba5b342b26209cdb25f2
Herbert <herbert.he0229@gmail.com>
2022-03-04 19:06:05 +0800

    feat :sparkles: : Update hug new

--- src/actions/runNewProject.ts
+++ src/actions/runNewProject.ts
@@ -1,4 +1,24 @@
+import prompts from "prompts"
+import { HugTemplates } from "../constants"
+
 export async function runNewProject() {
-	// TODO 生成新的项目
-	console.log("Hello Hug-cli!")
+    // TODO 生成新的项目
+    // 提示填写项目名称
+    const { projName } = await prompts({
+        type: "text",
+        name: "projName",
+        message: "Input the project name:",
+        initial: "hug-template",
+    })
+    console.log(projName)
+    // 提示选择项目模板
+    const { projTemplate } = await prompts({
+        type: "select",
+        name: "projTemplate",
+        message: "Choose the project template:",
+        choices: HugTemplates
+    })
+    console.log(projTemplate)
+    // 下载模板文件
+    // 提醒下载依赖
 }
---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const HugTemplates = [
+    {
+        title: "Vite + React + TypeScript Template",
+        value: "hug-template-vite-react-ts",
+        description: "Create Vite react-ts project"
+    },
+]
